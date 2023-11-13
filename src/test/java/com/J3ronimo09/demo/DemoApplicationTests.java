c15fa7222055f9e6cf5de9759116f0639f33fa06
Jeronimo Santos <jeronimo032001@hotmail.com>
2023-11-13 13:23:16 -0300

    Inplementing API Rest with java spring

---
+++ src/test/java/com/J3ronimo09/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.J3ronimo09.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
